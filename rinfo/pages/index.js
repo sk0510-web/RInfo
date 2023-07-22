21e8ee1db5ea94b0d2f9a395af6f559b66fec7c2
sk0510.web@gmail.com <sakuraikeisuke0510@icloud.com>
2023-07-23 07:05:03 +0900

    add map pin

--- rinfo/pages/index.js
+++ rinfo/pages/index.js
@@ -3,9 +3,19 @@ import axios from 'axios';
 import Head from 'next/head';
 import Link from 'next/link';
 import '../app/globals.css';
+import { GoogleMap, withGoogleMap, withScriptjs, Marker } from "react-google-maps";
 
 const API_KEY = '5e7653c1a566d6d9';
 const defaultEndpoint = `https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${API_KEY}&format=json`;
+
+const Map = withScriptjs(withGoogleMap((props) =>
+  <GoogleMap
+    defaultZoom={14}
+    defaultCenter={{ lat: props.latitude, lng: props.longitude }}
+    defaultOptions={{ disableDefaultUI: true }}
+    zoom={props.zoom}
+  />
+));
 
 export async function getServerSideProps() {
   const res = await fetch(defaultEndpoint);
@@ -19,10 +29,46 @@ export async function getServerSideProps() {
 }
 
 export default function Home({ data }) {
-
   const [latitude, setLatitude] = useState(null);
   const [longitude, setLongitude] = useState(null);
   const [shops, setShops] = useState([]);
+  const [searchRadius, setSearchRadius] = useState('20'); // Changed to a string value
+
+  const handleRadiusChange = (e) => {
+    const newRadius = e.target.value;
+    setSearchRadius(newRadius);
+    setZoom(calculateZoom(newRadius));  // Add this to calculate the zoom level based on radius
+  };
+
+  const calculateZoom = (radius) => {
+    // Add function to calculate the appropriate zoom level based on radius
+    return Math.max(1, Math.min(20, Math.round(14 - Math.log(radius) / Math.LN2)));
+  }
+
+  const CustomGoogleMap = withScriptjs(
+    withGoogleMap(props => (
+      <GoogleMap
+        defaultZoom={14}
+        defaultCenter={{ lat: props.latitude, lng: props.longitude }}
+        defaultOptions={{ disableDefaultUI: true }}
+        zoom={props.zoom}
+      >
+        {props.markers.map((marker, index) => (
+          <Marker key={index} position={{ lat: marker.lat, lng: marker.lng }} />
+        ))}
+      </GoogleMap>
+    ))
+  );
+
+  const getMarkers = () => {
+    return shops.map(shop => ({
+      lat: parseFloat(shop.lat),
+      lng: parseFloat(shop.lng)
+    }));
+  };
+
+  // マップ上のマーカーの位置情報を取得
+  const markers = getMarkers();
 
   useEffect(() => {
     if (navigator.geolocation) {
@@ -45,7 +91,7 @@ export default function Home({ data }) {
     const fetchDataByLocation = async () => {
       try {
         if (latitude && longitude) {
-          const response = await axios.get(`/api/hotpepper?lat=${latitude}&lng=${longitude}`);
+          const response = await axios.get(`/api/hotpepper?lat=${latitude}&lng=${longitude}&range=${searchRadius}`);
           const data = response.data;
           const shopData = data.results.shop;
           setShops(shopData);
@@ -56,7 +102,7 @@ export default function Home({ data }) {
     };
 
     fetchDataByLocation();
-  }, [latitude, longitude]);
+  }, [latitude, longitude, searchRadius]);
   
   const fetchData = async (lat, lng) => {
     try {
@@ -141,7 +187,7 @@ export default function Home({ data }) {
     const fetchData = async () => {
       try {
         if (latitude && longitude) { // latitudeとlongitudeが定義されていることを確認する
-          const response = await axios.get(`/api/hotpepper?lat=${latitude}&lng=${longitude}`);
+          const response = await axios.get(`/api/hotpepper?lat=${latitude}&lng=${longitude}&range=${searchRadius}`);
           const data = response.data;
   
           // レスポンスデータを適切に加工して状態を更新する処理を記述する
@@ -156,7 +202,7 @@ export default function Home({ data }) {
     };
   
     fetchData();
-  }, [latitude, longitude]);
+  }, [latitude, longitude, searchRadius]);
 
   const [page, updatePage] = useState({
     results_available: results_available,
@@ -178,11 +224,11 @@ export default function Home({ data }) {
     <>
       <Head>
         <link href="http://fonts.googleapis.com/earlyaccess/notosansjp.css" rel="stylesheet" />
-        <title>東京グルメ店検索</title>
+        <title>周辺レストラン検索</title>
       </Head>
 
       <div className="SearchFunc">
-        <header>
+      <header>
           <div className="Search">
             <div className="STitle">
               <h2 className="Title">ホットペッパー周辺レストラン検索</h2>
@@ -197,43 +243,58 @@ export default function Home({ data }) {
                 />
                 <button type="submit" className=""></button>
               </form>
-              <div className="SRNum">
-                <span>{page.results_available}</span> <span>件</span>
+              <div className="SRange">
+              <form action="" method="post" id="form" name = "local" class = "form">
+                        
+                        <select name="range" id = "date" >
+                            <option disabled selected value></option>
+                            
+                            <option value="1">300m</option>
+                            <option value="2">500m</option>
+                            <option value="3">1000m</option>
+                            <option value="4">2000m</option>
+                            <option value="5">3000m</option>
+
+                        </select>
+                        <a href="javascript:local.submit()" class = "btn-circle-stitch search_btn1" id = "button1">検索</a>
+                        <button type="submit" className=""></button>
+                    </form>
               </div>
             </div>
           </div>
         </header>
 
-        {currentLocation && (
-  <div className="map">
-    <iframe
-      src={`https://maps.google.com/maps?q=${currentLocation.latitude},${currentLocation.longitude}&output=embed`}
-      width="100%"
-      height="100%"
-      frameBorder="0"
-      style={{ border: 0 }}
-      allowFullScreen=""
-      aria-hidden="false"
-      tabIndex="0"
-    ></iframe>
-  </div>
-)}
+<div className='main'>
+  <div className='map'>
+{currentLocation && (
+          <CustomGoogleMap
+            latitude={currentLocation.latitude}
+            longitude={currentLocation.longitude}
+            zoom={calculateZoom(searchRadius) + 5}
+            markers={markers} // ここでマーカーの位置情報を渡す
+            googleMapURL={`https://maps.googleapis.com/maps/api/js?key=AIzaSyDSy5S3DrBEROlYOy2otaCKQrJhYkWC9QM&libraries=places`}
+            loadingElement={<div style={{ height: `100%` }} />}
+            containerElement={<div style={{ height: `100vh` }} />}
+            mapElement={<div style={{ height: `100%` }} />}
+          />
+        )}
+        </div>
 
-<footer>
-<section className="SRes" aria-label="Gallery">
+<div className="SRes">
+<span>{page.results_available}</span> <span>件</span>
   <div className="carousel">
     {shop && shop.length > 0 ? (
       <div className="RList__viewport">
         {shop.map((item, index) => (
           <div key={index} className="RList__slide1">
-            <Link href={item.urls.pc}>
+            <Link href={item.urls.pc} passHref target="_blank" rel="noopener noreferrer">
               <div className="RBox">
                 <div className="RBox1">
                   <img src={item.photo.mobile.s} alt={item.name} />
-                  <h3>{item.name}</h3>
                 </div>
                 <div className="RBox2">
                   <div>
+                  <h3>{item.name}</h3>
                     <div>
                       <span>{item.genre.name}</span>
                       <span>{item.catch}</span>
@@ -257,7 +318,12 @@ export default function Home({ data }) {
       </button>
     </div>
   )}
-</section>
+</div>
+
+</div>
+
+<footer>
+Powered by <a href="http://webservice.recruit.co.jp/">ホットペッパー Webサービス</a>
 </footer>
       </div>
     </>
