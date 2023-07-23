453fdd0f5e881c815d9240b23d9cc1f228916860
sk0510.web@gmail.com <sakuraikeisuke0510@icloud.com>
2023-07-23 19:16:26 +0900

    will add modal

--- rinfo/pages/index.js
+++ rinfo/pages/index.js
@@ -3,19 +3,120 @@ import axios from 'axios';
 import Head from 'next/head';
 import Link from 'next/link';
 import '../app/globals.css';
-import { GoogleMap, withGoogleMap, withScriptjs, Marker } from "react-google-maps";
+import { css } from '@emotion/react'
+import { GoogleMap, withGoogleMap, withScriptjs, Marker, InfoWindow } from "react-google-maps";
 
 const API_KEY = '5e7653c1a566d6d9';
 const defaultEndpoint = `https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${API_KEY}&format=json`;
 
-const Map = withScriptjs(withGoogleMap((props) =>
+const CustomGoogleMap = withScriptjs(withGoogleMap(props => (
   <GoogleMap
     defaultZoom={14}
     defaultCenter={{ lat: props.latitude, lng: props.longitude }}
     defaultOptions={{ disableDefaultUI: true }}
     zoom={props.zoom}
-  />
-));
+  >
+    {props.markers.map((marker, index) => (
+      <Marker key={index} position={{ lat: marker.lat, lng: marker.lng }} onClick={() => props.onMarkerClick(index)} />
+    ))}
+    {props.selectedMarker !== null && (
+      <InfoWindow
+        position={{ lat: props.markers[props.selectedMarker].lat, lng: props.markers[props.selectedMarker].lng }}
+        onCloseClick={() => props.onMarkerClick(null)}
+      >
+        <div className="map_item">
+          <img src={props.markers[props.selectedMarker].photo.pc.l} onClick={() => detail(props.selectedMarker)} />
+          <h3>{props.markers[props.selectedMarker].name}</h3>
+          <h6>{props.markers[props.selectedMarker].genre.name}</h6>
+          <h5>{props.markers[props.selectedMarker].genre.catch}</h5>
+          <p>{props.markers[props.selectedMarker].catch}</p>
+        </div>
+      </InfoWindow>
+    )}
+  </GoogleMap>
+)));
+
+function shopMap(locations) {
+  deleteMakers();
+  locations.forEach(function (element, index) {
+      var mapLatLng = new google.maps.LatLng(element['lat'], element['lng']);
+      var marker = new google.maps.Marker({
+          map: map,
+          position: mapLatLng
+      });
+      marker_array.push(marker);
+
+      // InfoWindowの内容を設定
+      var box = '<div class="map_item">' +
+          `<img src='${element['photo']['pc']['l']}' onclick='detail(${index})'>` +
+          `<h3>${element['name']}</h3>` +
+          `<h6>${element['genre']['name']}</h6>` +
+          `<h5>${element['genre']['catch']}</h5>` +
+          `<p>${element['catch']}</p>` +
+          '</div>';
+
+      var infowindow = new google.maps.InfoWindow({
+          content: box
+      });
+
+      // マーカーをクリックした際にInfoWindowを表示するように設定
+      marker.addListener('click', function () {
+          infowindow.open(map, marker);
+      });
+  });
+}
+
+function data_display(data) {
+  // 既存のコードをそのまま使用
+
+  // 各店の詳細情報を取得して表示する
+  data['shop'].forEach(function (element, index) {
+      var child_div = document.createElement("div");
+      child_div.setAttribute('onclick', `detail_close(${index})`);
+      child_div.classList.add('popup');
+      child_div.id = `shop-${index}`;
+      var content = document.createElement("div");
+      content.classList.add('content');
+
+      var h3 = document.createElement("h3");
+      h3.textContent = element['name'];
+      content.appendChild(h3);
+
+      var p = document.createElement("p");
+      p.style.textAlign = "center";
+      p.textContent = element['catch'];
+      content.appendChild(p);
+
+      var div = document.createElement("div");
+      div.style.textAlign = "center";
+      var img = document.createElement("img");
+      img.setAttribute('src', element['photo']['pc']['l']);
+      div.appendChild(img);
+      content.appendChild(div);
+
+      var detail_area = document.createElement("div");
+      detail_area.classList.add('detail_area');
+
+      var table = document.createElement("table");
+      table.classList.add('detail_table');
+
+      // その他の店の詳細情報をここで設定する（住所、営業時間、料金など）
+
+      detail_area.appendChild(table);
+      content.appendChild(detail_area);
+
+      var close = document.createElement("a");
+      close.classList.add('close');
+      var icon = document.createElement("i");
+      icon.classList.add('fas', 'fa-times-circle');
+      close.appendChild(icon);
+      close.setAttribute('href', `javascript: detail_close(${index})`);
+      content.appendChild(close);
+
+      child_div.appendChild(content);
+      details.appendChild(child_div);
+  });
+}
 
 export async function getServerSideProps() {
   const res = await fetch(defaultEndpoint);
@@ -33,6 +134,13 @@ export default function Home({ data }) {
   const [longitude, setLongitude] = useState(null);
   const [shops, setShops] = useState([]);
   const [searchRadius, setSearchRadius] = useState('20'); // Changed to a string value
+  const [searchResults, setSearchResults] = useState([]);
+  const [zoom, setZoom] = useState(14);
+  const [selectedMarker, setSelectedMarker] = useState(null);
+
+  const handleMarkerClick = (index) => {
+    setSelectedMarker(index);
+  };
 
   const handleRadiusChange = (e) => {
     const newRadius = e.target.value;
@@ -95,6 +203,8 @@ export default function Home({ data }) {
           const data = response.data;
           const shopData = data.results.shop;
           setShops(shopData);
+
+          setSearchResults(shopData);
         }
       } catch (error) {
         console.error('ホットペッパーAPIからのデータ取得に失敗しました', error);
@@ -104,18 +214,25 @@ export default function Home({ data }) {
     fetchDataByLocation();
   }, [latitude, longitude, searchRadius]);
   
-  const fetchData = async (lat, lng) => {
-    try {
-      const response = await axios.get(`/api/hotpepper?lat=${lat}&lng=${lng}`);
-      const data = response.data;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        if (latitude && longitude) {
+          const response = await axios.get(`/api/hotpepper?lat=${latitude}&lng=${longitude}&range=${searchRadius}`);
+          const data = response.data;
+          const shopData = data.results.shop;
+          updateShops(shopData);
 
-      // レスポンスデータを適切に加工して状態を更新する処理を記述する
-      const shopData = data.results.shop;
-      setShops(shopData);
-    } catch (error) {
-      console.error('ホットペッパーAPIからのデータ取得に失敗しました', error);
-    }
-  };
+          // ピンを表示するために、検索結果の店情報をsearchResultsにセットする
+          setSearchResults(shopData);
+        }
+      } catch (error) {
+        console.error('ホットペッパーAPIからのデータ取得に失敗しました', error);
+      }
+    };
+
+    fetchData();
+  }, [latitude, longitude, searchRadius]);
 
   useEffect(() => {
     if (navigator.geolocation) {
@@ -232,6 +349,9 @@ export default function Home({ data }) {
           <div className="Search">
             <div className="STitle">
               <h2 className="Title">ホットペッパー周辺レストラン検索</h2>
+              <div className='credit'>
+              Powered by <br></br><a href="http://webservice.recruit.co.jp/">ホットペッパー Webサービス</a>
+              </div>
             </div>
             <div className="SBox">
               <form onSubmit={handlerOnSubmitSearch} id="form" className="form">
@@ -246,17 +366,16 @@ export default function Home({ data }) {
               <div className="SRange">
               <form action="" method="post" id="form" name = "local" class = "form">
                         
-                        <select name="range" id = "date" >
-                            <option disabled selected value></option>
+              <select name="range" id="date" value={searchRadius} onChange={handleRadiusChange}>
+                      <option value="" disabled>選択してください</option>
                             
                             <option value="1">300m</option>
                             <option value="2">500m</option>
                             <option value="3">1000m</option>
-                            <option value="4">2000m</option>
-                            <option value="5">3000m</option>
+                            <option value="4">3000m</option>
 
                         </select>
-                        <a href="javascript:local.submit()" class = "btn-circle-stitch search_btn1" id = "button1">検索</a>
+                        <a href="local.submit()" class = "btn-circle-stitch search_btn1" id = "button1">検索</a>
                         <button type="submit" className=""></button>
                     </form>
               </div>
@@ -264,25 +383,27 @@ export default function Home({ data }) {
           </div>
         </header>
 
-<div className='main'>
+<main>
   <div className='map'>
-{currentLocation && (
-          <CustomGoogleMap
-            latitude={currentLocation.latitude}
-            longitude={currentLocation.longitude}
-            zoom={calculateZoom(searchRadius) + 5}
-            markers={markers} // ここでマーカーの位置情報を渡す
-            googleMapURL={`https://maps.googleapis.com/maps/api/js?key=AIzaSyDSy5S3DrBEROlYOy2otaCKQrJhYkWC9QM&libraries=places`}
-            loadingElement={<div style={{ height: `100%` }} />}
-            containerElement={<div style={{ height: `100vh` }} />}
-            mapElement={<div style={{ height: `100%` }} />}
-          />
-        )}
+            {currentLocation && (
+              <CustomGoogleMap
+                latitude={currentLocation.latitude}
+                longitude={currentLocation.longitude}
+                zoom={calculateZoom(searchRadius) + 5}
+                markers={searchResults} // ここでマーカーの位置情報を渡す
+                selectedMarker={selectedMarker} // 選択されたマーカーのインデックスを渡す
+                onMarkerClick={handleMarkerClick} // マーカークリック時のハンドラーを渡す
+                googleMapURL={`https://maps.googleapis.com/maps/api/js?key=AIzaSyCJNPlapAMB7Fg3udUECkCvphXmJrKViKM&libraries=places`}
+                loadingElement={<div style={{ height: `100%` }} />}
+                containerElement={<div style={{ height: `100vh` }} />}
+                mapElement={<div style={{ height: `100%` }} />}
+              />
+            )}
         </div>
 
 <div className="SRes">
-<span>{page.results_available}</span> <span>件</span>
   <div className="carousel">
+    <h2>検索結果一覧</h2><br></br>
     {shop && shop.length > 0 ? (
       <div className="RList__viewport">
         {shop.map((item, index) => (
@@ -295,7 +416,7 @@ export default function Home({ data }) {
                 <div className="RBox2">
                   <div>
                   <h3>{item.name}</h3>
-                    <div>
+                    <div className='detail'>
                       <span>{item.genre.name}</span>
                       <span>{item.catch}</span>
                     </div>
@@ -311,20 +432,8 @@ export default function Home({ data }) {
       <div></div>
     )}
   </div>
-  {page.results_available > page.results_start && (
-    <div className="">
-      <button className="" onClick={handlerOnClickReadMore}>
-        もっと読む
-      </button>
-    </div>
-  )}
 </div>
-
-</div>
-
-<footer>
-Powered by <a href="http://webservice.recruit.co.jp/">ホットペッパー Webサービス</a>
-</footer>
+</main>
       </div>
     </>
   );
